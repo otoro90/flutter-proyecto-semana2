953c162d1826dab9b3e772573217078cae9daab7
Oscar Miguel Toro Torres <omigueltoro@gmail.com>
2021-04-25 23:03:01 -0500

    Primer avance de maquetación

--- lib/page/home-page.dart
+++ lib/page/home-page.dart
@@ -3,15 +3,6 @@ import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -23,64 +14,200 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+                height: 160,
+                width: 160,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF212122),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            new Tab(
+                                icon: new Image.asset("assets/female.png",
+                                    height: 40),
+                                text: "female")
+                          ])
+                    ])),
+            Container(
+                height: 160,
+                width: 160,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF212122),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            new Tab(
+                                icon: new Image.asset("assets/male.png",
+                                    height: 40),
+                                text: "male")
+                          ])
+                    ])),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add, color: Colors.white, size: 32),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Expanded(
+              child: Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF212122),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  height: 160,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              SliderTheme(
+                                data: SliderThemeData(
+                                    activeTrackColor: Colors.white,
+                                    thumbColor: Colors.pink,
+                                    overlayColor: Colors.pink.withOpacity(0.2),
+                                    inactiveTrackColor: Colors.white24,
+                                    valueIndicatorColor: Colors.pink),
+                                child: Slider(
+                                  value: 166,
+                                  min: 50,
+                                  max: 200,
+                                  label: "Estatura",
+                                  onChanged: (double value) {},
+                                ),
+                              )
+                            ])
+                      ]))),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+                height: 160,
+                width: 160,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF212122),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [Text("Edad")]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "38",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 50),
+                            )
+                          ]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            FloatingActionButton(
+                              onPressed: _incrementCounter,
+                              child: Icon(Icons.add,
+                                  color: Colors.white, size: 32),
+                            ),
+                            FloatingActionButton(
+                              onPressed: _incrementCounter,
+                              child: Icon(Icons.add,
+                                  color: Colors.white, size: 32),
+                            )
+                          ])
+                    ])),
+            Container(
+                height: 160,
+                width: 160,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF212122),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [Text("Edad")]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              "38",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 50),
+                            )
+                          ]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            FloatingActionButton(
+                              onPressed: _incrementCounter,
+                              child: Icon(Icons.add,
+                                  color: Colors.white, size: 32),
+                            ),
+                            FloatingActionButton(
+                              onPressed: _incrementCounter,
+                              child: Icon(Icons.add,
+                                  color: Colors.white, size: 32),
+                            )
+                          ])
+                    ])),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(0.0),
+          child: Container(
+              height: 80,
+              color: Colors.pink,
+              child: Column(children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Text(
+                          "Calcular",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 20),
+                        ),
+                      ),
+                    ])
+              ])),
+        ),
+      ],
     );
   }
 }
